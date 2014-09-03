8e1c889a0d10fb23d9d31809520802e125bf8105
puffnstuff <jppuffnstuff@gmail.com>
2014-09-03 14:31:14 -0400

    More Development

--- ticTocToe.js
+++ ticTocToe.js
@@ -2,6 +2,8 @@
 
   $(function () {
 
+    $("#tic-toc-toe").append('<div id="header"></div><div id="push-messages"><div id="message"></div><div id="move-timer">3<br />Seconds Left Until Random Choice!</div><div id="times-won"></div></div><div id="playable"><div id="board"></div></div>');
+
     var game = new ticTockToe();
     game.new();
 
@@ -21,7 +23,7 @@ var ticTockToe = function () {
   var options = {
 
     // How big the board will be...
-    boardSize : 3,
+    boardSize : 5,
 
     // Duration of a move before a random move (ms)...
     moveTimeout : 3000, // 3.0 Sec
@@ -33,11 +35,11 @@ var ticTockToe = function () {
     player1Mark : "x",
     player2Mark : "o",
 
-    fadeDuration:  800, // 0.8 Sec
+    fadeDuration: 1000, // 1.0 Sec
     pushDuration: 5000, // 5.0 Sec
 
     nextGameTimeout: 5, // In seconds, 5.0 Sec
-    nextMoveTimeout: 3  // 3.0 Sec
+    nextMoveTimeout: 1  // 3.0 Sec
 
   } // End options Object
 
@@ -46,13 +48,13 @@ var ticTockToe = function () {
 
     push: function (msg, strClasses, sticky) {
 
-      $("#push-messages #message").stop().fadeOut(options.fadeDuration, function (e) {
+      $("#tic-toc-toe #push-messages #message").stop().fadeOut(options.fadeDuration, function (e) {
         $(this).html(msg).removeClass().addClass('push-' + (strClasses || 'notice')).fadeIn(options.fadeDuration);
       });
 
       if(!sticky) {
         pushTimeout = setTimeout(function () {
-          $("#push-messages #message").fadeOut(options.fadeDuration);
+          $("#tic-toc-toe #push-messages #message").fadeOut(options.fadeDuration);
           clearTimeout(pushTimeout);
         }, options.pushDuration);
       }
@@ -61,14 +63,14 @@ var ticTockToe = function () {
 
     append: function (msg, strClasses, sticky) {
 
-      $("#push-messages #message").hide(function (e) {
+      $("#tic-toc-toe #push-messages #message").hide(function (e) {
         $(this).html($(this).html() + '<br />' + msg).removeClass().addClass('push-' + (strClasses || 'notice')).fadeIn(options.fadeDuration);
       });
 
       clearTimeout(pushTimeout);
       if(!sticky) {
         pushTimeout = setTimeout(function () {
-          $("#push-messages #message").fadeOut(options.fadeDuration);
+          $("#tic-toc-toe #push-messages #message").fadeOut(options.fadeDuration);
           clearTimeout(pushTimeout);
         }, options.pushDuration);
       }
@@ -76,7 +78,7 @@ var ticTockToe = function () {
     }, // End append()
 
     clear: function () {
-      $("#push-messages #message").html("");
+      $("#tic-toc-toe #push-messages #message").html("");
     } // End clear()
 
   } // End message Object
@@ -90,7 +92,8 @@ var ticTockToe = function () {
   var moveCount = 0;
   var turn      = undefined;
   var gameOver  = false;
-  var lastMove    = [];
+  var lastMove  = [];
+  var timesWon  = [0, 0, 0];
 
 
   var game = {
@@ -134,13 +137,17 @@ var ticTockToe = function () {
     self.new();
   }
 
+  self.stop = function () {
+    clearInterval(nextGameTimeout);
+  }
+
 
   // <------------------------- PRIVATE METHODS ------------------------> //
 
   function setMoveTimer() {
     var moveTimer = options.nextMoveTimeout;
     moveInterval = setInterval(function () {
-      $("#move-timer").html(moveTimer + "<br />Seconds left Until Random Choice!");
+      $("#tic-toc-toe #move-timer").html(moveTimer + "<br />Seconds Left Until Random Choice!");
       
       if(moveTimer <= 0) {
         clearInterval(moveInterval);
@@ -154,21 +161,20 @@ var ticTockToe = function () {
     
     var row = Math.floor(Math.random() * (options.boardSize));
     var col = Math.floor(Math.random() * (options.boardSize));
-    console.log(movesRaw.indexOf([row, col, 1]));
 
-    while(movesRaw.indexOf([row, col, 1]) == -1) {
+    while((movesRaw.indexOf('r:' + row + 'c:' + col + 'p:' + 1) > -1) || (movesRaw.indexOf('r:' + row + 'c:' + col + 'p:' + 2) > -1)) {
       row = Math.floor(Math.random() * (options.boardSize));
       col = Math.floor(Math.random() * (options.boardSize));
-      console.log(row, col, turn, movesRaw, movesRaw.indexOf([row, col, 1]));
-      if(movesRaw.indexOf([row, col, 1]) == -1) break;
     }
+    $('#tic-toc-toe #board table tr td.row-' + row + '.col-' + col).html('<div class="player-' + turn + '-mark">' + ((turn == 1) ? options.player1Mark : options.player2Mark + "</div>"));
 
     moves[turn].push({ row: row, col: col });
-    movesRaw.push([row, col, turn]);
-
+    movesRaw.push('r:' + row + 'c:' + col + 'p:' + turn);
+    lastMove = [row, col];
+    moveCount++;
     gameOver = winner();
     if(!gameOver) {
-      nextTurn();
+      nextTurn(true);
       setMoveTimer();
     }
   }
@@ -181,24 +187,32 @@ var ticTockToe = function () {
     turn      = undefined;
     gameOver  = false;
 
+    $('#tic-toc-toe #times-won *').remove();
+
+    $('#tic-toc-toe #times-won').append('<div>' + options.player1Name + ': <span id="wp-' + 1 + '">' + timesWon[1] + '</span></div>');
+    $('#tic-toc-toe #times-won').append('<div>' + options.player2Name + ': <span id="wp-' + 2 + '">' + timesWon[2] + '</span></div>');
+    $('#tic-toc-toe #times-won').append('<div>' + 'Stale Mate: ' + '<span id="wp-0">' + timesWon[0] + '</span></div>');
+
+
     $('.board-col').html("").removeClass("winning-move");
+    $('#tic-toc-toe #move-timer').show();
 
     // Determine if Player 1 || 2 goes first:
     turn = Math.floor(Math.random() * (3 - 1)) + 1;
     message.push(game.currentPlayer + " has been randomly choosen to go first!", "notice");
 
-    
     setMoveTimer();
 
-    $('#game-page #board table tr td').click(function () {
+    $('#tic-toc-toe #board table tr td').click(function () {
+      clearInterval(moveInterval);
       if($(this).html() == '') {
-        $(this).html('<div class="player' + turn + '-mark">' + ((turn == 1) ? options.player1Mark : options.player2Mark + "</div>"));
+        $(this).html('<div class="player-' + turn + '-mark">' + ((turn == 1) ? options.player1Mark : options.player2Mark + "</div>"));
         
         var row = $(this).attr('class').match(/row-(\d+)/)[1];
         var col = $(this).attr('class').match(/col-(\d+)/)[1];
 
         moves[turn].push({ row: row, col: col });
-        movesRaw.push([row, col, turn]);
+        movesRaw.push('r:' + row + 'c:' + col + 'p:' + turn);
         lastMove = [row, col];
         moveCount++;
         gameOver = winner();
@@ -222,47 +236,68 @@ var ticTockToe = function () {
   } // End setOptions()
 
 
-  function nextTurn () {
+  function nextTurn (compMove) {
     turn = (turn == 1) ? 2 : 1;
-    message.push("You made your mark in row " + (parseInt(lastMove[0]) + 1) + ", column " + (parseInt(lastMove[1]) + 1) + ". Now, it's " + ((turn == 1) ? options.player1Name : options.player2Name) + "'s turn.", "notice", true);
+    message.push(((compMove) ? "<strong>The Computer</strong>" : "You") + " made your mark in row " + (parseInt(lastMove[0]) + 1) + ", column " + (parseInt(lastMove[1]) + 1) + ". Now, it's " + ((turn == 1) ? options.player1Name : options.player2Name) + "'s turn.", "notice", true);
   }
 
 
   function winner () {
 
+    var winningPlayer = undefined;
+    var loosingPlayer = undefined;
+
     if(moveCount == Math.pow(options.boardSize, 2)) {
       message.push("Stale Mate! There's no spots left. Game Over.", 'notice', true);
       gameOver = true;
+      wp = 0;
+      loosingPlayer = null;
     }
     else {
 
-      var winningPlayer = undefined;
-      var loosingPlayer = undefined;
-
-      console.log(moves);
-
       // Determine if there is a winning combination:
       gameOver = (turn == 1) ? determineWinner(1) : determineWinner(2);
-      winningPlayer = (turn == 1) ? options.player1Name : options.player2Name;
-      loosingPlayer = (turn == 1) ? options.player2Name : options.player1Name;
 
-      if(gameOver) message.push(winningPlayer + " has defeated Player " + loosingPlayer + "!", 'notice', true);
+      if(gameOver) {
+        wp = (turn == 1) ? 1 : 2;
+
+        winningPlayer = (turn == 1) ? options.player1Name : options.player2Name;
+        loosingPlayer = (turn == 1) ? options.player2Name : options.player1Name;
+        message.push(winningPlayer + " has defeated Player " + loosingPlayer + "!", 'notice', true);
+      }
 
     }
 
     if(gameOver) {
-      message.append('<span id="play-again">Next game in <span id="timer">' + options.nextGameTimeout + '</span></span>', 'notice', true);
+      $('#tic-toc-toe #move-timer').fadeOut(options.fadeDuration);
+      timesWon[wp] = parseInt($('#tic-toc-toe #times-won #wp-' + wp).html());
+      $('#tic-toc-toe #times-won #wp-' + wp).html(++timesWon[wp]);
+      message.append('<span id="play-again">Next game in <span id="timer">' + options.nextGameTimeout + '</span><span id="quit">Tired of playing? Quit.</span></span>', 'notice', true);
       var timeToNextGame = options.nextGameTimeout;
       var nextGameTimeout = setInterval(function () {
+
+        $("#tic-toc-toe #timer").html(timeToNextGame);
+
+        $('#tic-toc-toe #quit').click(function () {
+          $("#tic-toc-toe #move-timer").hide();
+          $("#tic-toc-toe #push-messages *").toggle(1000, function () {
+            $(this).remove();
+          });
+          $("#tic-toc-toe #times-won *").toggle(1000, function () {
+            $(this).remove();
+          });
+
+          clearInterval(nextGameTimeout);
+        });
+
         timeToNextGame--;
-        if(timeToNextGame <= 0) {
+        if(timeToNextGame <= -1) {
           clearInterval(nextGameTimeout);
           reset();
           message.clear();
         }
-        $("#timer").html(timeToNextGame);
       }, 1000);
-      $('#game-page #board table tr td').unbind('click');
+      $('#tic-toc-toe #board table tr td').unbind('click');
       
     }
 
@@ -273,6 +308,8 @@ var ticTockToe = function () {
 
   function determineWinner (player) {
 
+    if(movesRaw.length < 5) return false;
+
     var cols = {};
     var rows = {};
 
@@ -280,37 +317,54 @@ var ticTockToe = function () {
       cols[moves[player][i].col] = !cols[moves[player][i].col] ? 1 : ++cols[moves[player][i].col];
       rows[moves[player][i].row] = !rows[moves[player][i].row] ? 1 : ++rows[moves[player][i].row];
 
-      console.log(cols, rows);
     }
 
-    var diags = 0;
+    var diagDown = 0;
+    var diagUp = 0;
+    var diagDownWinners = [];
+    var diagUpWinners = [];
+    var l = options.boardSize - 1;
     for(var i = 0; i < options.boardSize; i++) {
       if((cols[i] && cols[i] >= options.boardSize)) {
-        $('#board .col-' + i).addClass('winning-move');
+        $('#tic-toc-toe #board .col-' + i).addClass('winning-move');
         return true;
       }
       if(rows[i] && rows[i] >= options.boardSize) {
-        $('#board .row-' + i).addClass('winning-move');
+        $('#tic-toc-toe #board .row-' + i).addClass('winning-move');
         return true;
       }
 
-      // WORK ON THIS
-      if(cols[i] && cols[i] >= 1 && rows[i] && rows[i] >= 1) {
-
-        diags++;
-        if(diags >= options.boardSize) {
-          
-          for(var n = options.boardSize; n >= 0; n--) {
-            $('#board .row-' + n + '.col-' + n).addClass('winning-move');
-          }
-          return true;
+      
+      for(var k in movesRaw) {
+        if(movesRaw[k] == 'r:' + i + 'c:' + i + 'p:' + turn) {
+          diagDown++;
+          diagDownWinners.push([i, i]);
+        }
+        console.log('r:' + i + 'c:' + l + 'p:' + turn);
+        if(movesRaw[k] == 'r:' + i + 'c:' + l + 'p:' + turn) {
+          diagUp++;
+          diagUpWinners.push([i, l]);
         }
       }
+      l--;
+    }
 
+    if(diagDown >= options.boardSize) {
+      for(var i in diagDownWinners) {
+        console.log(diagDownWinners);
+        $('#tic-toc-toe #board .row-' + diagDownWinners[i][0] + '.col-' + diagDownWinners[i][1]).addClass('winning-move');
+      }
+      return true;
+    }
+    else if(diagUp >= options.boardSize) {
+      for(var i in diagUpWinners) {
+        console.log(diagUpWinners);
+        $('#tic-toc-toe #board .row-' + diagUpWinners[i][0] + '.col-' + diagUpWinners[i][1]).addClass('winning-move');
+      }
+      return true;
     }
 
     return false;
-   // console.log(cols, rows);
 
   } // End determineWinner()
 
@@ -328,7 +382,9 @@ var ticTockToe = function () {
     } // End for loop
 
     board.push("</tbody></table>");
-    $("#game-page #board").html(board.join(''));
+    $("#tic-toc-toe #board").html(board.join(''));
+    $("#tic-toc-toe #board table tr td").attr('style', 'width:' + 100/options.boardSize + '%; height:' + $("#tic-toc-toe #board table").height()/options.boardSize + 'px !important;');
+
 
   } // End buildBoard()
 
